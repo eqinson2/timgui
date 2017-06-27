a1aef34507bae7377725ff5b998264d686ac8b8a
qingwei song <qingwei.song@ericsson.com>
2017-06-27 21:39:31 +0800

    backend

---
+++ TIM-GUI-Backend/src/main/scala/com/ericsson/ema/timgui/SetServlet.scala
@@ -0,0 +1,54 @@
+package com.ericsson.ema.timgui
+
+import com.ericsson.ema.tim.context.TableInfoMap
+import com.ericsson.ema.tim.dml.Update
+import com.ericsson.ema.tim.dml.predicate.Eq
+import org.scalatra.{BadRequest, InternalServerError, NoContent, ScalatraServlet}
+import org.slf4j.{Logger, LoggerFactory}
+import play.api.libs.json.Json
+
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Created by eqinson on 2017/6/26.
+  */
+class SetServlet extends ScalatraServlet with JsonErrorMessage {
+	private val logger: Logger = LoggerFactory.getLogger(getClass)
+
+	private val defaultHeader = Map("Content-Type" -> "application/json;UTF-8")
+
+	put("/:tableName") {
+		params.get("tableName") match {
+			case Some(table) if validTables.isValid(table) =>
+				val fields = TableInfoMap().lookup(table).map(_.tableMetadata.keys.toList)
+
+				fields match {
+					case Some(f) =>
+						val json = Json.parse(request.body)
+						val oldData = (json \ "oldData").validate[Map[String, String]].get
+						val newData = (json \ "newData").validate[Map[String, String]].get
+
+						val updater = Update().into(table)
+
+						for ((newField, newValue) <- newData; if newValue != "")
+							updater.set(newField, newValue)
+
+						for ((oldField, oldValue) <- oldData)
+							updater.where(Eq(oldField, oldValue))
+
+						Try(updater.execute()) match {
+							case Success(_)  => NoContent
+							case Failure(ex) => BadRequest(body = jsonResponse("500", "update into table failed"), reason = ex.getMessage)
+						}
+					case None    =>
+						InternalServerError(body = jsonResponse("500", s"unexpected table: $table in zkcache when set table"), headers = defaultHeader)
+				}
+
+			case Some(table) if !validTables.isValid(table) =>
+				BadRequest(body = jsonResponse("400", s"illegal table name: $table in set request"), reason = "Bad Request")
+
+			case None =>
+				BadRequest(body = jsonResponse("400", "illegal set request"), reason = "Bad Request")
+		}
+	}
+}
